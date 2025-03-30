eb5c49981d9a6d9711fe07b70ee25d97b0d722cd
avdmr <89525698+avdmr@users.noreply.github.com>
2025-03-30 18:20:46 +0200

    Update run.sh

--- bitfocus-companion/run.sh
+++ bitfocus-companion/run.sh
@@ -1,14 +1,11 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
-# Haal configuratie op uit options
-CONFIG_DIR=$(bashio::config 'config_dir')
+# Definieer een vaste config directory
+CONFIG_DIR="/companion"
 
 # Zorg ervoor dat de config directory bestaat
 mkdir -p ${CONFIG_DIR}
 chown companion:companion ${CONFIG_DIR}
-
-# Log bericht
-bashio::log.info "Starting Bitfocus Companion..."
 
 # Start Companion met de nodige parameters
 exec /docker-entrypoint.sh ./node-runtimes/main/bin/node ./main.js --admin-address 0.0.0.0 --admin-port 8000 --config-dir ${CONFIG_DIR} --extra-module-path /app/module-local-dev
