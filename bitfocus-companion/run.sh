5ad7509f103a173a8e3f3ac7b9044d43c8248358
avdmr <89525698+avdmr@users.noreply.github.com>
2025-03-30 23:19:03 +0200

    Update run.sh

--- bitfocus-companion/run.sh
+++ bitfocus-companion/run.sh
@@ -8,16 +8,6 @@ echo "Creating persistent storage directory..."
 mkdir -p /data/companion
 chmod 777 /data/companion
 
-# Bepaal architectuur voor de juiste image
-if [ "$(uname -m)" = "aarch64" ]; then
-  echo "Detected ARM64 architecture"
-  IMAGE="ghcr.io/bitfocus/companion/companion:3.5.3-7770-stable-df70e20b@sha256:ff126d7fa635ae9f64568d522432f7665dc8c846f067302b39ae55eb513472a5"
-else
-  echo "Detected AMD64 architecture"
-  IMAGE="ghcr.io/bitfocus/companion/companion:3.5.3-7770-stable-df70e20b@sha256:813dfd0f40a570f2fd1a4e390edd9e19fa21c790c3b2068af54213f1e2c2f2cf"
-fi
-
-# Gebruik directe socat om poorten door te sturen
 echo "Starting port forwarding..."
 # Start socat processen op de achtergrond om poorten door te sturen
 socat TCP-LISTEN:8000,fork,reuseaddr TCP:127.0.0.1:18000 &
