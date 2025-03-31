a42eef02b79580817ad62a516de261f5bf6e4b41
avdmr <89525698+avdmr@users.noreply.github.com>
2025-03-31 21:37:27 +0200

    Update run.sh

--- bitfocus-companion/run.sh
+++ bitfocus-companion/run.sh
@@ -4,7 +4,7 @@ set -e
 # Zorg dat de /companion map bestaat
 mkdir -p /companion
 
-# Als de persistente opslag (/data) leeg is, kopieer de standaardconfiguratie (indien aanwezig)
+# Als de persistente opslag (/data) leeg is, kopieer dan de standaardconfiguratie (indien aanwezig)
 if [ ! -d "/data" ] || [ -z "$(ls -A /data)" ]; then
   echo "Kopieer standaardconfiguratie naar /data"
   if [ -d "/companion/v3.5" ]; then
@@ -19,4 +19,11 @@ rm -rf /companion/v3.5
 ln -s /data /companion/v3.5
 
 echo "Start Companion..."
+echo "Huidige PATH: $PATH"
+
+# Stel het PATH expliciet opnieuw in zodat de node binary wordt gevonden
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/app/node-runtimes/main/bin"
+echo "Aangepaste PATH: $PATH"
+
+# Voer het originele opstartcommando uit
 exec "$@"
