044a25e558ea22df2f59ed484f0fdd2d0b0adaba
avdmr <89525698+avdmr@users.noreply.github.com>
2025-03-31 21:07:08 +0200

    Update run.sh

--- bitfocus-companion/run.sh
+++ bitfocus-companion/run.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 set -e
 
-# Controleer of de persistente opslag (/data) leeg is.
+# Zorg ervoor dat de directory /companion bestaat
+mkdir -p /companion
+
+# Als de persistente opslag (/data) leeg is, kopieer de standaardconfiguratie
 if [ ! -d "/data" ] || [ -z "$(ls -A /data)" ]; then
   echo "Kopieer standaard configuratie naar /data"
-  cp -r /companion/v3.5/* /data/
+  if [ -d "/companion/v3.5" ]; then
+    cp -r /companion/v3.5/* /data/
+  else
+    echo "Geen standaard configuratie gevonden in /companion/v3.5."
+  fi
 fi
 
 # Verwijder de originele configuratiemap en maak een symlink naar /data
 rm -rf /companion/v3.5
 ln -s /data /companion/v3.5
 
-# Start de Companion applicatie
+echo "Start Companion..."
 exec companion "$@"
